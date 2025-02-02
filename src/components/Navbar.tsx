c13911cf684ecf77fd2a3eb0b2c7710fe570fd68
DamRandom <britohdamian@gmail.com>
2025-02-01 20:49:04 -0500

    Created Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 export default function Navbar() {
-    return (
-      <nav className="fixed top-0 left-0 w-full bg-[#596766] text-white p-4 flex justify-between items-center">
-        <h1 className="text-xl font-bold">PowerHaus</h1>
-        <ul className="flex space-x-4">
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#team">Team</a></li>
-        </ul>
-      </nav>
-    );
-  }
-  
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-[#DADCE1] text-[#262520] p-4 flex justify-between items-center shadow-md shadow-[#262520] z-10">
+      {/* Nombre del negocio */}
+      <h1 className="text-xl font-semibold">PowerHaus</h1>
+
+      {/* Menú de navegación */}
+      <ul className="flex space-x-6">
+        <li><a href="#about" className="hover:text-[#ABC1BB]">About</a></li>
+        <li><a href="#services" className="hover:text-[#ABC1BB]">Services</a></li>
+        <li><a href="#team" className="hover:text-[#ABC1BB]">Team</a></li>
+      </ul>
+    </nav>
+  );
+}
