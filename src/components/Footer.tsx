a42943935499f38a4be1c6d153b156fab2b6a51b
DamRandom <britohdamian@gmail.com>
2025-02-09 18:07:59 -0500

    Clean Footer component code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,20 +3,21 @@ import { FaInstagram, FaFacebook } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer
-      id='footer'
-      className="w-full bg-[#DADCE1] text-[#262520] p-4 flex justify-between items-center shadow-md shadow-[#262520]">
-      {/* Columna izquierda: nombre del negocio y eslogan */}
+      id="footer"
+      className="w-full bg-[#DADCE1] text-[#262520] p-4 flex justify-between items-center shadow-md shadow-[#262520]"
+    >
+      {/* Left column: Business name and slogan */}
       <div className="flex flex-col space-y-1">
         <h1 className="text-xl font-semibold">PowerHaus</h1>
         <p className="text-sm">Making your home smarter, one appliance at a time.</p>
       </div>
 
-      {/* Columna central: derechos de autor */}
+      {/* Center column: Copyright notice */}
       <div className="flex-1 flex justify-center">
         <p className="text-sm">&copy; {new Date().getFullYear()} PowerHaus. All rights reserved.</p>
       </div>
 
-      {/* Columna derecha: íconos de redes sociales */}
+      {/* Right column: Social media icons */}
       <div className="flex space-x-4">
         <a href="https://www.instagram.com/powerhaus" className="text-2xl hover:text-[#ABC1BB]">
           <FaInstagram />
