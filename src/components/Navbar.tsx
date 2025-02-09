0d2a039641f5a6c49465609f28b72dffe9d09811
DamRandom <britohdamian@gmail.com>
2025-02-09 18:10:05 -0500

    Clean Navbar component code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-[#DADCE1] text-[#262520] p-4 flex justify-between items-center shadow-md shadow-[#262520] z-10">
-      {/* Nombre del negocio */}
+      {/* Business name */}
       <h1 className="text-xl font-semibold">PowerHaus</h1>
 
-      {/* Menú de navegación */}
+      {/* Navigation menu */}
       <ul className="flex space-x-6">
         <li><a href="#about" className="hover:text-[#ABC1BB]">About</a></li>
         <li><a href="#services" className="hover:text-[#ABC1BB]">Services</a></li>
