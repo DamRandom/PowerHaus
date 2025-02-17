185301422394c32b0cdd29887d70bb1d4f5386a7
DamRandom <britohdamian@gmail.com>
2025-02-17 00:46:48 -0500

    Updated styles in search bar

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -15,7 +15,7 @@ export default function Filters({ categories, onCategorySelect, onClearFilters }
   return (
     <div className="bg-tertiary px-6 py-4 mb-8">
       {/* Filters Card */}
-      <div className="bg-white p-4 rounded-lg shadow-md mb-6">
+      <div className="bg-white p-4 rounded-lg shadow-md h-15 mb-6">
         <div className="flex justify-between items-center border-b pb-2">
           <h2 className="text-lg font-medium text-gray-700">Filters</h2>
           <button
